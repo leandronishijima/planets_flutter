9c5c2b79eab5bc5b1cf22cc7729f5aeeb00685f2
Leandro Nishijima <leandro.nishijima@gmail.com.br>
2019-01-23 11:40:03 -0200

    iniital commit + header with gradient

---
+++ lib/HomePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'GradientAppBar.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[GradientAppBar('treva')],
+      ),
+    );
+  }
+}
