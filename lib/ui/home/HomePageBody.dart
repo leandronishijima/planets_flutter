11ae1f9a1222f40f96152995da5124a1197ff4e6
Leandro Nishijima <leandro.nishijima@gmail.com.br>
2019-01-24 09:42:26 -0200

    refactoring ListView -> CustomScrollView (with Silver)

--- lib/ui/home/HomePageBody.dart
+++ lib/ui/home/HomePageBody.dart
@@ -9,9 +9,22 @@ class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: ListView.builder(
-      itemBuilder: (context, index) => PlanetRow(planets[index]),
-      itemCount: planets.length,
+        child: Container(
+      color: Color(0xFF736AB7),
+      child: CustomScrollView(
+        scrollDirection: Axis.vertical,
+        slivers: <Widget>[
+          SliverPadding(
+            padding: EdgeInsets.symmetric(vertical: 24.0),
+            sliver: SliverFixedExtentList(
+              itemExtent: 162.0,
+              delegate: SliverChildBuilderDelegate(
+                  (context, index) => PlanetRow(planets[index]),
+                  childCount: planets.length),
+            ),
+          )
+        ],
+      ),
     ));
   }
 }
