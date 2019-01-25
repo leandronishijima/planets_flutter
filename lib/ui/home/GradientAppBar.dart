716fe8cea41e7d81835795d308282244e3e90861
leandronishijima <leandro.nishijima@gmail.com.br>
2019-01-25 11:46:39 -0200

    new icons on AppBar on body page

--- lib/ui/home/GradientAppBar.dart
+++ lib/ui/home/GradientAppBar.dart
@@ -11,26 +11,35 @@ class GradientAppBar extends StatelessWidget {
     final double statusBarHeight = MediaQuery.of(context).padding.top;
 
     return Container(
-      padding: EdgeInsets.only(top: statusBarHeight),
-      height: statusBarHeight + barHeight,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: [const Color(0xFF3366FF), const Color(0xFF00CCFF)],
-            begin: FractionalOffset(0.0, 0.0),
-            end: FractionalOffset(1.0, 0.0),
-            stops: [0.0, 1.0],
-            tileMode: TileMode.clamp),
-      ),
-      child: Center(
-        child: Text(
-          title,
-          style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.w600,
-              fontSize: 36.0),
+        padding: EdgeInsets.only(top: statusBarHeight, left: 20.0, right: 20.0),
+        height: statusBarHeight + barHeight,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [const Color(0xFF3366FF), const Color(0xFF00CCFF)],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp),
         ),
-      ),
-    );
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                    fontSize: 36.0),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.white,
+              )
+            ]));
   }
 }
