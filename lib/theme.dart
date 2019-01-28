776d5992bbe6220f73c78f1de50d1f632e4a6a45
Leandro Nishijima <leandro.nishijima@gmail.com>
2019-01-28 08:44:53 -0200

    test commit 2

---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Theme {
+  static TextStyle title() {
+    return TextStyle(
+        color: Colors.white,
+        fontFamily: 'Poppins',
+        fontWeight: FontWeight.w600,
+        fontSize: 36.0);
+  }
+}
